a1331f223cb6cadf750c55e69b1665f466088cda
Seryang <seryang@live.co.kr>
2016-01-26 21:54:23 +0900

    # 16.01.26 - file upload

---
+++ src/main/java/com/teamcloud/controller/loginController.java
@@ -0,0 +1,68 @@
+package com.teamcloud.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.teamcloud.model.service.OauthService;
+import com.teamcloud.model.vo.UserVO;
+
+@Controller
+//@SessionAttributes("userInfo")
+public class loginController {
+
+	@Autowired
+	private OauthService oauthService;
+
+	@RequestMapping(value="/", produces="text/plain;charset=UTF-8")
+	public String index(HttpSession session){
+		String url = "redirect:/login.est";
+		try{
+			System.out.println("user info :" + session.getAttribute("userInfo") );
+			if(session.getAttribute("userInfo") != null){
+				url = "index";
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("url : " + url);
+		return url;
+	}
+
+	@RequestMapping(value="/login.est", produces="text/plain;charset=UTF-8" )
+	public String login() {
+		String loginPage = oauthService.getUriPath().toString();
+		System.out.println("로그인 페이지 !" + loginPage);
+		return "redirect:" + loginPage;
+	}
+
+	@RequestMapping(value="/authorization.est", produces="text/plain;charset=UTF-8")
+	public String authorization( @RequestParam("code") String code, Model model ) {
+		System.out.println("여긴 autorization");
+		String url = "redirect:/login.est";
+		try{
+			JSONObject tokenInfo = oauthService.getToken(code);
+			if(tokenInfo != null){
+				UserVO user = oauthService.getUserInfo(tokenInfo);
+				System.out.println(user.toString());
+				model.addAttribute("userInfo", user);
+				url = "index";
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return url;
+	}
+
+	@RequestMapping(value="/logout.est", produces="text/plain;charset=UTF-8")
+	public String authorization(HttpSession session, Model model) {
+		session.invalidate();
+		model.asMap().clear();
+		return "redirect:/";
+	}
+}
